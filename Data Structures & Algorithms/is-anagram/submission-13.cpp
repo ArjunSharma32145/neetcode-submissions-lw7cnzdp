cd434f7147773bc70c294c58f5fdf990b6907d39
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-04-14 22:32:19 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> freq(26,0);
+        for(auto c : s) freq[c-'a']++;
+        for(auto c : t) freq[c-'a']--;
+        for(auto it : freq){
+         if(it != 0) return false;
+        } 
+        return true;
+    }
+};
