674934eefb8b795dcb85f51abe249cd0f7be494d
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-05-13 01:04:21 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+       sort(nums.begin(),nums.end());
+        unordered_map<int,int> mpp;
+        int n = nums.size();
+        int req = n/3;
+        int check = INT_MIN;
+        for(auto it : nums){
+            if(check == it ) continue;
+            mpp[it]++;
+            if(mpp[it] > n/3){
+                ans.push_back(it);
+                check = it;
+            }
+        }
+        return ans;
+    }
+};
