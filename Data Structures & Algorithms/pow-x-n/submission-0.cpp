2dea6ce824a4dcf3fb18906ed99f332719353750
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-05-19 16:55:48 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+         if( n%2 == 0){
+            double half = myPow(x,n/2);
+            return half*half;
+        }
+        if(n<0){
+        return (1/x)*myPow(x,n+1);
+        }
+        return x*myPow(x,n-1); 
+    }
+};
