d7f3409b8772eec53d5aac0e20a2c5c09eccfc90
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-05-23 09:56:08 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int k = s1.size();
+        int n = s2.size();
+        unordered_map<int,int> track;
+        for(auto it : s1) track[it]++;
+        unordered_map<int,int> freq;
+        int left = 0;
+        for(int i=0;i<n;i++){
+          freq[s2[i]]++;
+          if(i-left+1 == k){
+            if(track == freq) return true;
+            freq[s2[left]]--;
+            if(freq[s2[left]] == 0) freq.erase(s2[left]);
+            left++;
+          }
+         
+        }
+      return false;
+    }
+};
