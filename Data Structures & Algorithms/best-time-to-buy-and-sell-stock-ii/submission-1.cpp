2c75e33a2f6e171a73e1a11027c6ab2f233a935d
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-02 16:43:13 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int maxiprof = 0;
+        for(int i=1;i<prices.size();i++){
+            int sell = prices[i];
+            int prof = sell - buy;
+            if(prof >0 ) {
+                maxiprof += prof;
+            }
+             buy = sell; 
+        }
+        return maxiprof;
+    }
+};
