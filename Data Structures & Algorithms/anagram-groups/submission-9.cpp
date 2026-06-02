f9d54ea473fe2c68a4d146cc12e84c15a62b1612
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-02 14:35:10 +0530

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mpp;
+        for(auto s : strs){
+            string sortedS = s;
+            sort(sortedS.begin(),sortedS.end());
+            mpp[sortedS].push_back(s);
+        }
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
