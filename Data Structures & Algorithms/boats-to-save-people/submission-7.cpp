3cc798535a2689090971c4f3413739c3dc7d251c
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-03 21:00:08 +0530

    Add: boats-to-save-people - submission-7

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int i =0;
+        int j = n-1;
+        int boats= 0;
+        sort(people.begin(),people.end());
+        while(i<=j){
+            int weight =people[i] + people[j];
+            if( weight<= limit){
+                i++;
+                j--;
+                boats++;
+            }
+            else{
+                j--;
+                boats++;
+            }
+        }
+        return boats;
+    }
+};
