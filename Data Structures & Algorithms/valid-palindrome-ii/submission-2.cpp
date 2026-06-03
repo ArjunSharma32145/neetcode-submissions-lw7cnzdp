8573d8e099cfdf8b488758f4888a02e6b2f59cf9
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-03 09:04:12 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    bool palind(int left,int right,string & s){
+        while(left<right){
+            if(s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+       int n = s.size();
+       int i=0;
+       int j=n-1;
+       while(i<j){
+        if(s[i] != s[j]){
+            bool oneway = palind(i+1,j,s);
+            bool secway = palind(i,j-1,s);
+            return oneway || secway;
+        }
+        i++;
+        j--;
+       }
+       return true;
+    }
+};
