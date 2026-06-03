8a82c4e11e7b9e8d7ad58bfb6a3da7d745ba505f
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-03 09:21:12 +0530

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // piche se bharo simple
+        int p = m+n-1;
+        int i=m-1;
+        int j=n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[p] = nums1[i];
+                p--;
+                i--;
+            }
+            else{
+                nums1[p] = nums2[j];
+                p--;
+                j--;
+            }
+        }
+        while(i>=0){
+            nums1[p] = nums1[i];
+            p--;
+            i--;
+        }
+        while(j>=0){
+            nums1[p] = nums2[j];
+            p--;
+            j--;
+        }
+
+    }
+};
