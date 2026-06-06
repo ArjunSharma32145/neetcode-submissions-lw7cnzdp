f81e47f433b0d876f13ddc53930d8e4e4bc11020
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-06 06:16:05 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string ans = "";
+        if(!root) return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+         ans += to_string(root->val);
+            ans += ',';
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            if(node->left){
+               q.push(node->left);
+              ans += to_string(node->left->val);
+              ans += ',';
+            }
+            else{
+                 ans += "null";
+                 ans += ',';
+            }
+            if(node->right){
+                q.push(node->right);
+                ans += to_string(node->right->val);
+                ans += ',';
+            }
+            else {
+                ans += "null";
+                ans +=',';
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size() == 0 ) return NULL;
+        stringstream s(data);
+        string str;
+        getline(s,str,',');
+        TreeNode * root = new TreeNode(stoi(str));
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            getline(s,str,',');
+            if(str == "null"){
+                node->left = NULL;
+             }
+             else{
+                TreeNode * left = new TreeNode(stoi(str));
+                node->left = left;
+                q.push(left); 
+             }
+             getline(s,str,',');
+             if(str == "null"){
+                node->right =  NULL;
+             }
+             else{
+                TreeNode * right = new TreeNode(stoi(str));
+                node->right = right;
+                q.push(right);
+             }
+        }
+        return root;
+    }
+};
