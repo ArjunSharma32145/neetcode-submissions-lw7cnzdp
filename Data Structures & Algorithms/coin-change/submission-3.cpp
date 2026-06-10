56b92f71a3cfec93e0a273fca34c8faa5145677d
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-10 22:39:48 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // two thing changing index as well as amount hence 2D dp
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,1e9));
+      //  dp[0][kuch bhi] = 0; // right
+     //   dp[kuch bhi][0] = 0; //right so these are base cases
+     for(int i=0;i<=n;i++) dp[i][0] = 0; // no need any coin
+     for(int j=0;j<=amount;j++) dp[0][j] = 1e9; // this just never possible
+
+  // unlimited no of each coin hence unbounded knapsack
+     for(int i=1;i<=n;i++){
+      for(int j=1;j<=amount;j++){
+        if(coins[i-1] <= j){
+          dp[i][j] = min(dp[i-1][j],dp[i][j-coins[i-1]] + 1);
+        }
+        else dp[i][j] = dp[i-1][j];
+      }
+     }
+   if(dp[n][amount] == 1e9) return -1;
+   return dp[n][amount];
+
+    }
+};
