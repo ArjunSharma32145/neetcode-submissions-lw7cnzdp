859abb7b7dcfa0b33a0fcc70a2c5df4ca6b80011
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-10 22:52:29 +0530

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int minprod = nums[0];
+        int maxprod = nums[0];
+        int result = nums[0];
+        for(int i=1;i<nums.size();i++){
+          if(nums[i] <0) swap(minprod,maxprod);
+          minprod = min(nums[i],minprod*nums[i]);
+          maxprod = max(nums[i],maxprod*nums[i]);
+          result = max(result,maxprod);
+        }
+        return result;
+    }
+};
