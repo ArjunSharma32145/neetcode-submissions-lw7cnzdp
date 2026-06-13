cd373c7a24303cccb039c50edae843f84992082f
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-13 14:45:10 +0530

    Add: edit-distance - submission-7

---
+++ Data Structures & Algorithms/edit-distance/submission-7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+         // if word1 empty then insert all chars of word2
+         for(int j=0;j<=n;j++) dp[0][j] = j;
+         // if word2 empty then remove all chars of word1
+         for(int i=0;i<=m;i++) dp[i][0] = i;
+
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+
+                if(word1[i-1] == word2[j-1]){
+                   dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert = dp[i][j-1] + 1;
+                    int del = dp[i-1][j] + 1;
+                    int replace  = dp[i-1][j-1] + 1;
+                    dp[i][j] = min(replace,min(insert,del));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
