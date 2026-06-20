80d67d3f9f59be7ebd5ac7698c6769f89100cbae
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-20 12:11:31 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    bool palind(string check){
+        int n = check.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+           if(check[i] != check[j]) return false;
+           i++;
+           j--;
+        }
+        return true;
+    }
+    void solve(int index,string & s,vector<string> & temp,vector<vector<string>> & ans){
+
+        if(index == s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        string lol = "";
+        for(int i=index;i<s.size();i++){
+             lol += s[i];
+             if(palind(lol)){
+                temp.push_back(lol);
+                solve(i+1,s,temp,ans);
+                temp.pop_back();
+             }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0,s,temp,ans);
+        return ans;
+
+    }
+};
