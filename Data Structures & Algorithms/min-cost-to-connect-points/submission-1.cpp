777817d1288dfd8662f2674669477bae3ea1cf42
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-06-29 18:38:37 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    static const int N = 1001;
+    int parent[N];
+    int size[N];
+
+    void make(int v){
+        parent[v] = v;
+        size[v] = 1;
+    }
+
+    int find(int v){
+        if(parent[v] == v) return v;
+        //path compression
+        return parent[v] = find(parent[v]);
+    }
+
+    void Union(int a,int b){
+        a = find(a);
+        b= find(b);
+        if( a != b){
+            if(size[a] < size[b]) swap(a,b);
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+    vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<points.size();i++){
+            for(int j=0;j<points.size();j++){
+                if(i == j) continue;
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int cost = abs(x1-x2) + abs(y1-y2);
+                edges.push_back({cost,{i,j}}); 
+            }
+        }
+        for(int i=0;i<points.size();i++) make(i);
+        int ans = 0;
+        sort(edges.begin(),edges.end());
+        for(auto it : edges){
+         int u = it.second.first;
+         int v = it.second.second;
+         int cost = it.first;
+         if(find(u) == find(v)) continue;
+         Union(u,v);
+         ans += cost;
+        }
+        return ans;
+    }
+};
