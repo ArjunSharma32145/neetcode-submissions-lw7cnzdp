a8d41357f091b4e9a200cdc5fd70df51d95a15d9
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-07-11 19:37:02 +0530

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // har anagram ki key mein uska sorted array bana sakta hu
+        unordered_map<string,vector<string>> mpp;
+        for(auto s : strs){
+            string sortedS = s;
+            sort(sortedS.begin(),sortedS.end());
+            mpp[sortedS].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
