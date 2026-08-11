e97c44127e3f21662aa1bbb25e2539f7d3a18a0b
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-08-11 10:33:09 +0530

    Add: maximum-twin-sum-of-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int> arr;
+        ListNode * lol = head;
+        while(lol){
+             arr.push_back(lol->val);
+             lol = lol->next;
+        }
+        int i=0;
+        int j = arr.size()-1;
+        int ans= 0;
+        while(i<j){
+          ans= max(ans,arr[i] + arr[j]);
+          i++;
+          j--;
+        }
+        return ans;
+    }
+};
