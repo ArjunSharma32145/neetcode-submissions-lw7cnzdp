7240efde5141b7f0316ebb5a60b551b65059b80d
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-09-08 15:09:29 +0530

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> freq;
+        for(auto it : s) freq[it]++;
+        for(auto it : t){
+            freq[it]--;
+            if(freq[it] < 0) return false;
+        }
+        return true;
+    }
+};
