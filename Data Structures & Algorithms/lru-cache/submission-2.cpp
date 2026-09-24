1f2d05e3e28a019dd07d42fcdbc3921091253996
ArjunSharma32145 <168546693+ArjunSharma32145@users.noreply.github.com>
2026-09-24 11:21:28 +0530

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.cpp
@@ -0,0 +1,37 @@
+class LRUCache {
+public:
+      vector<pair<int,int>> cache;
+      int cap;
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        for(int i=0;i<cache.size();i++){
+            if(cache[i].first == key){
+                pair<int,int> temp = cache[i];
+                cache.erase(cache.begin() + i);
+                cache.push_back(temp);
+                return temp.second;
+            }
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        for(int i=0;i<cache.size();i++){
+            if(cache[i].first == key){
+                pair<int,int> temp = cache[i];
+                cache.erase(cache.begin() + i);
+                temp.second = value;
+                cache.push_back(temp);
+                return;
+            }
+        }
+        if(cache.size() >= cap){
+            cache.erase(cache.begin());
+        }
+        cache.push_back({key,value});
+        return;
+    }
+};
